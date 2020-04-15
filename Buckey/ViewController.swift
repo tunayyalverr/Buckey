51190287cbaf13515524af642c92c2afa297461c
tunay alver <tunay.alver@mobillium.com>
2020-04-15 22:41:44 +0300

    Initial Commit

---
+++ Buckey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Buckey
+//
+//  Created by tunay alver on 15.04.2020.
+//  Copyright © 2020 tunay alver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
